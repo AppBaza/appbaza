3f9305025e443b45481d1afaec124f16f98637d9
Kanan Rahimov <mail@kenanbek.me>
2026-01-27 07:32:11 +0100

    chore: update version label to V0.7.0 in changelog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -169,7 +169,7 @@ const config = {
                 to: '/blog',
               },
               {
-                label: 'V0.6.0 (Sep 2023)',
+                label: 'V0.7.0 (Jan 2026)',
                 href: '/changelog',
               },
               {
